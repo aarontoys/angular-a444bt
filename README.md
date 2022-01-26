c2ae41612222cd9b11128feb0bfb46af99319e8a
aarontoys <aarontoys@gmail.com>
2022-01-26 10:24:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-a444bt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a444bt)
---
+++ src/test-files.ts
@@ -0,0 +1,8 @@
+// Import spec files individually for Stackblitz
+import './app/user-greeting.component.spec';
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
